5621cfb195c20e96c3eee9beba86108e4a989cd8
alioxp <alioxp@gmail.com>
2016-04-23 07:00:50 +0800

    use cast

--- MyApplication/app/src/main/java/config/GlobalConfig.java
+++ MyApplication/app/src/main/java/config/GlobalConfig.java
@@ -4,7 +4,7 @@ package config;
  * Created by lishan on 2016/4/22.
  */
 public class GlobalConfig {
-    public static final boolean USE_BROADCAST_UDP = false;
+    public static final boolean USE_BROADCAST_UDP = true;
     public static String BROADCAST_UDP_IP = "224.0.1.88";
     public static int UDP_PORT = 30000;
     public static String NORMAL_UDP_IP = "192.168.95.1";
